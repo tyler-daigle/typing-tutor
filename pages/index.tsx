41fb9d41fac7201c36c76e8ea183ee6fc7746000
Tyler D <tyler@tdaigle.com>
2023-06-03 13:31:08 -0400

    removed single quotes from the text, it causes firefox's quick search to popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import TypingTimer from "./components/TypingTimer";
 
 export default function Index() {
   const currentText =
-    "The quick brown fox jumps over the lazy dog. This sentence contains all the letters of the English alphabet. However, some letters appear more frequently than others. For example, the letter 'e' is the most commonly used letter in English, while the letters 'q', 'x', and 'z' are relatively rare. Learning to type efficiently requires practice, but it can greatly improve your productivity and reduce the risk of repetitive strain injuries.";
+    'The quick brown fox jumps over the lazy dog. This sentence contains all the letters of the English alphabet. However, some letters appear more frequently than others. For example, the letter "e" is the most commonly used letter in English, while the letters "q", "x", and "z" are relatively rare. Learning to type efficiently requires practice, but it can greatly improve your productivity and reduce the risk of repetitive strain injuries.';
   const [typedText, setTypedText] = useState<string>("");
   const nextChar = currentText.charAt(typedText.length).toLowerCase();
   const [timerRunning, setTimerRunning] = useState(false);
